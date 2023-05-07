01820f8e6206f2edb5962df3a1dbf4eaeaf051a7
Alchera-Sam <nullterra180@gmail.com>
2023-05-08 03:42:49 +0900

    chore: remove config file typo in userPoolId

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
   cognito: {
-    userPoolId: "ap-northeast-2_tzcYsZS5D	",
+    userPoolId: "ap-northeast-2_tzcYsZS5D",
     userPoolClientId: "2io047fon7so4u6cqmjptr7hn1",
     region: "ap-northeast-2"
   },
